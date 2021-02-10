759f35e9247f2133a77f68e9287d5b021bb016a1
Zihao <zihaozhang1@gmail.com>
2021-02-09 21:50:37 -0500

    Port fix

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require ('./data/animals')
 
@@ -52,7 +54,7 @@ app.get('/api/animals', (req, res) => {
   res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
 
