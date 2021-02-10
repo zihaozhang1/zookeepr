db31131c912edaea76bbfeeee67d7fda5383d54b
Zihao <zihaozhang1@gmail.com>
2021-02-09 21:53:08 -0500

    heroku port change

--- server.js
+++ server.js
@@ -55,6 +55,6 @@ app.get('/api/animals', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
+  console.log(`API server now on port ${PORT}!`);
+});
 
